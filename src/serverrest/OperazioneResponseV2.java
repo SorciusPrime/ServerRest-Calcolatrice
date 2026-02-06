3df0b8b197425d25df7b024bb281bc1be3fd8470
Grilli Nico <150380049+SorciusPrime@users.noreply.github.com>
2026-02-06 10:31:23 +0100

    creazione delle v2

---
+++ src/serverrest/OperazioneResponseV2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serverrest;
+
+/**
+ *
+ * @author grilli.nico
+ */
+public class OperazioneResponseV2 {
+    
+}
