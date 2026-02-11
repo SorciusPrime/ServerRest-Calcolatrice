1f242356608a3cc4039764d9eebe038ee3675317
Grilli Nico <150380049+SorciusPrime@users.noreply.github.com>
2026-02-11 08:56:47 +0100

    inizio implementazione v2

--- src/serverrest/OperazioneResponseV2.java
+++ src/serverrest/OperazioneResponseV2.java
@@ -6,8 +6,74 @@ package serverrest;
 
 /**
  *
- * @author grilli.nico
+ * @author delfo
  */
 public class OperazioneResponseV2 {
+    private double operando1;
+    private double operando2;
+    private String operatore;
+    private double risultato;
+    private String operazione;
+    private String timestamp; 
+    private String versione_api;
+    private String request_id;
     
+    // Costruttore vuoto necessario per GSON
+    public OperazioneResponseV2() {
+    }
+   
+    // Costruttore con parametri
+    public OperazioneResponseV2(double operando1, double operando2, 
+                             String operatore, double risultato) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+        this.operatore = operatore;
+        this.risultato = risultato;
+        this.operazione = String.format("%.2f %s %.2f = %.2f", 
+            operando1, operatore, operando2, risultato);
+        
+        this.timestamp.LocalDateTime.now().format();
+    }
+    
+    // Getter
+    public double getOperando1() {
+        return operando1;
+    }
+    
+    public double getOperando2() {
+        return operando2;
+    }
+    
+    public String getOperatore() {
+        return operatore;
+    }
+    
+    public double getRisultato() {
+        return risultato;
+    }
+    
+    public String getOperazione() {
+        return operazione;
+    }
+    
+    // Setter
+    public void setOperando1(double operando1) {
+        this.operando1 = operando1;
+    }
+    
+    public void setOperando2(double operando2) {
+        this.operando2 = operando2;
+    }
+    
+    public void setOperatore(String operatore) {
+        this.operatore = operatore;
+    }
+    
+    public void setRisultato(double risultato) {
+        this.risultato = risultato;
+    }
+    
+    public void setOperazione(String operazione) {
+        this.operazione = operazione;
+    }
 }
